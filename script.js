b9ba71cef0f6a4011d24acb7ee4f79c35e6bc0aa
spacedcafe <137644150+spacedcafe@users.noreply.github.com>
2023-06-24 22:51:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+const idValue = document.getElementById("id-value");
+var today = new Date();
+var date = today.getDay() + "/" + today.getMonth() + "/" + today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//Object that stores values of minimum and maximum angle for a value
+const rotationValues = [
+  { minDegree: 0, maxDegree: 30, value: "Noodles" },
+  { minDegree: 31, maxDegree: 90, value: "Mangoshake" },
+  { minDegree: 91, maxDegree: 150, value: "French Fries" },
+  { minDegree: 151, maxDegree: 210, value: "Burger" },
+  { minDegree: 211, maxDegree: 270, value: "Momos" },
+  { minDegree: 271, maxDegree: 330, value: "Mint Mojito" },
+  { minDegree: 331, maxDegree: 360, value: "Noodles" },
+
+  // { minDegree: 0, maxDegree: 30, value: 2 },
+  // { minDegree: 31, maxDegree: 90, value: 1 },
+  // { minDegree: 91, maxDegree: 150, value: 6 },
+  // { minDegree: 151, maxDegree: 210, value: 5 },
+  // { minDegree: 211, maxDegree: 270, value: 4 },
+  // { minDegree: 271, maxDegree: 330, value: 3 },
+  // { minDegree: 331, maxDegree: 360, value: 2 },
+
+];
+//Size of each piece
+const data = [10, 10, 10, 10, 10, 10];
+//background color for each piece
+var pieColors = [
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+  "#8b35bc",
+  "#b163da",
+];
+//Create chart
+let myChart = new Chart(wheel, {
+  //Plugin for displaying text on pie chart
+  plugins: [ChartDataLabels],
+  //Chart Type Pie
+  type: "pie",
+  data: {
+    //Labels(values which are to be displayed on chart)
+    //labels: [1, 2, 3, 4, 5, 6],
+    labels: ["Mangoshake","Noodles","Mint Mojito", "Momos","Burger","French Fries"],
+    //Settings for dataset/pie
+    datasets: [
+      {
+        backgroundColor: pieColors,
+        data: data,
+      },
+    ],
+  },
+  options: {
+    //Responsive chart
+    responsive: true,
+    animation: { duration: 0 },
+    plugins: {
+      //hide tooltip and legend
+      tooltip: false,
+      legend: {
+        display: false,
+      },
+      //display labels inside pie chart
+      datalabels: {
+        color: "#ffffff",
+        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+        font: { size: 14 },
+      },
+    },
+  },
+});
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotationValues) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = `<p>You have won: ${i.value} <br>Time now: ${time}<br>Today's date: ${date}</p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+
+//Spinner count
+let count = 0;
+//100 rotations for animation and last rotation for result
+let resultValue = 101;
+let i=0;
+function closeMe()
+{
+  
+  open(location, '_self').close();
+}
+//Start spinning
+if(i==0){
+spinBtn.addEventListener("click", () => {
+        spinBtn.disabled = true;
+        //Empty final value
+        finalValue.innerHTML = `<p>Good Luck!</p>`;
+        //Generate random degrees to stop at
+        let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+        //Interval for rotation animation
+        let rotationInterval = window.setInterval(() => {
+            //Set rotation for piechart
+            /*
+            Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+            */
+            myChart.options.rotation = myChart.options.rotation + resultValue;
+            //Update chart with new value;
+            myChart.update();
+            //If rotation>360 reset it back to 0
+            if (myChart.options.rotation >= 360) {
+            count += 1;
+            resultValue -= 5;
+            myChart.options.rotation = 0;
+            } else if (count > 15 && myChart.options.rotation == randomDegree) {
+            valueGenerator(randomDegree);
+            clearInterval(rotationInterval);
+            count = 0;
+            resultValue = 101;
+            }
+        }, 10);
+        i++;   
+});
+}
+else{
+    // finalValue.innerHTML = `<p>Enough for today!  ${deviceID}</p>`;
+    closeMe(); 
+}
